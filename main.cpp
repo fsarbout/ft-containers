a1ba4100da47bc4a52b74e699bbc202bc48ef12d
fsarbout <fatimazahrasarbout@gmail.com>
2022-03-27 14:22:46 +0000

    clean

--- main.cpp
+++ main.cpp
@@ -1,24 +1,9 @@
 #include "Iterators/Iterator.hpp"
-#include <vector>
+#include "vector/Vector.hpp"
+
+
 
 int main()
 {
-    ft::iterator<int> it1;
-    ft::iterator<int> it2;
 
-    std::cout << it1 <<  std::endl;
-    it1++;
-    it1--;
-    ++it1;
-    --it1;
-    std::cout << (it1 == it2) << std::endl;
-    std::cout << (it1 + it2) << std::endl;
-    std::cout << it1 << std::endl;
-
-    // ft::Iterator<int> *it11 = NULL;
-
-    std::cout << it1 + 34543 << std::endl;
-    std::cout <<  34543 + it1  << std::endl;
-
-    // std::cout << it11->ptr << std::endl; 
 }
